cae2645bad4b6da2790e7281955204e508c8432b
Jeremy Milledge <jez.milledge@gmail.com>
2019-12-15 22:13:55 +1100

    seeing a basket yay

--- src/utils/Drawing.js
+++ src/utils/Drawing.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import styled from '@emotion/styled';
 
 const StyledSVG = styled.svg`
-  stroke: #ccc;
+  stroke: #222;
   stroke-width: 5;
+  fill: none;
+  padding: 5px;
+  box-model:  border-box;
 `;
 
 export default class Drawing extends Component {
@@ -11,22 +14,21 @@ export default class Drawing extends Component {
     super(props);
     this.state = {
       path: this.buildPath(),
-      width: 250,
-      height: 250
+      width: 265,
+      height: 265
     }
   }
 
   buildPath() {
     let data = '';
     for (let stroke in this.props.drawing) {
-
       this.props.drawing[stroke].forEach((p, i) => {
         let newPath = 'M'
         p[i].forEach((point, j) => {
           if (j !== 0) { //we already have the Move key for the first pair
             newPath += 'L'
           }
-          newPath += point.x + ',' + point.y
+          newPath += (point.x + 5) + ',' + (point.y + 5)
           if (j !== p[i].length - 1) {
             newPath += ','
           }
